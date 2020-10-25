221bae4390a7bd16b335b42444bb4a89381712d1
Itsusinn <itsusinn@meowcat.org>
2020-10-26 01:16:00 +0800

    initally commit

---
+++ front-end/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <NewComment title="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import NewComment from './components/NewComment.vue'
+
+export default {
+  name: 'app',
+  components: {
+    NewComment
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front-end/src/components/NewComment.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="16"><h2>评论</h2></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="16">
+        <el-input
+            type="textarea"
+            :rows="5"
+            placeholder="不说点什么吗"
+            v-model="content">
+        </el-input></el-col>
+    </el-row>
+
+    <el-row>
+      <el-col :span="4"><h5>昵称</h5></el-col>
+      <el-col :span="6"><h5>邮箱</h5></el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <el-input v-model="nick"></el-input>
+      </el-col>
+      <el-col :span="8">
+        <el-input v-model="email"></el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary" v-on:click="submit">提交</el-button>
+      </el-col>
+
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewComment',
+  props:{
+    title:String
+  },
+  data(){
+    return{
+      content:"",
+      nick:"",
+      email:"",
+      result:""
+    }
+  },
+  methods:{
+    submit:function () {
+      this.send()
+    },
+    send:function () {
+      this.axios
+          .get("http://127.0.0.1:8081/json/gson")
+          .then(function (respond) {
+            alert(respond.toString())
+          })
+          .catch(function (error){
+            console.log(error)
+          })
+    }
+  }
+}
+</script>
