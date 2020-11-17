f9942cb99e9a8fe8c43886ab4951618036624d3e
Itsusinn奕䜣 <itsusinn@foxmail.com>
2020-11-17 22:03:57 +0800

    refactor

---
+++ mychat-frontend-vue/src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue"
+import App from "./App.vue"
+
+import "./plugins/element.js"
+import "./plugins/axios.js"
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
